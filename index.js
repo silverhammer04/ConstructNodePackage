457a9bcdd93778b8c0c272ac83640d5ef3dd4528
Maxwell Seifert <silverhammer04@yahoo.com>
2020-09-04 08:35:34 -0600

    first commit

---
+++ index.js
@@ -0,0 +1,7 @@
+const color = require('colors');
+
+// text is green
+console.log('Hi World!'.green);
+
+// text is black and has a white background 
+console.log(color.inverse('This is the other way to do it.'));
